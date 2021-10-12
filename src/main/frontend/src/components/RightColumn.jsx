69e855cccb908b01e721a3ab1c46b216db9610b7
Sergei Leshchenko <leshchenko.sd@gmail.com>
2021-10-12 04:42:00 -0700

    Add profile and logout component.

--- src/main/frontend/src/components/RightColumn.jsx
+++ src/main/frontend/src/components/RightColumn.jsx
@@ -1,12 +1,14 @@
 import LoginComp from './Login';
 import { Route } from "react-router";
 import RegisterComp from './Register';
+import Profile from "./Profile";
 
 const RightColumn = () => {
     return(
         <div className="right_column">
            <Route path="/register" component={RegisterComp} />
            <Route path="/login" component={LoginComp} />
+            <Route path="/auth/success" component={Profile} />
         </div>
     );
 }
