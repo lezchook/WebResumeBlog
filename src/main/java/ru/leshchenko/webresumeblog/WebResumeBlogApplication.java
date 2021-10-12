adc07edb0c7c1eb0f4738c413a7ff8c21be2157a
Sergei Leshchenko <leshchenko.sd@gmail.com>
2021-10-12 03:51:21 -0700

    Base structure of ReactJS + Spring Boot app is ready.

---
+++ src/main/java/ru/leshchenko/webresumeblog/WebResumeBlogApplication.java
@@ -0,0 +1,13 @@
+package ru.leshchenko.webresumeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebResumeBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebResumeBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/leshchenko/webresumeblog/WebResumeBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.leshchenko.webresumeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebResumeBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
