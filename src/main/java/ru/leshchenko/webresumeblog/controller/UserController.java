7ea0a775145c436c22e7c56811113b7fd1e22656
Sergei Leshchenko <leshchenko.sd@gmail.com>
2021-10-16 00:27:47 +0300

    Posts are now stored in the database and are also obtained from it, and not from the local memory of the browser.

--- src/main/java/ru/leshchenko/webresumeblog/controller/UserController.java
+++ src/main/java/ru/leshchenko/webresumeblog/controller/UserController.java
@@ -1,11 +1,15 @@
 package ru.leshchenko.webresumeblog.controller;
 
+import com.fasterxml.jackson.core.JsonParser;
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.leshchenko.webresumeblog.domain.Post;
 import ru.leshchenko.webresumeblog.service.PostService;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -17,5 +21,13 @@ public class UserController {
     @PostMapping("/post")
     public void setPost(@RequestBody Post post ) {
         postService.savePost(post);
+        JSONArray jsonA = new JSONArray().put(postService.getAllPost());
+        System.out.println(jsonA);
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getPosts() {
+        JSONArray jsonA = new JSONArray().put(postService.getAllPost());
+        return postService.getAllPost();
     }
 }
