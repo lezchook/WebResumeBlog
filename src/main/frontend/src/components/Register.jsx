da8f47ef5c3da4c3c09a3e8c4a9c881558b15f58
Sergei Leshchenko <leshchenko.sd@gmail.com>
2021-10-15 22:00:57 +0300

    Registration of users is working!

--- src/main/frontend/src/components/Register.jsx
+++ src/main/frontend/src/components/Register.jsx
@@ -1,16 +1,42 @@
 import React from "react";
+import axios from "axios";
 class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.usernameChange = this.usernameChange.bind(this);
+        this.passwordChange = this.passwordChange.bind(this);
+    }
+
+    state = {
+        username: "",
+        password: "",
+    };
+
+    ClickButton = () => {
+        const user = {
+            username: this.state.username,
+            password: this.state.password,
+        }
+        axios.post('http://localhost:8080/auth/register', user);
+        console.log(user)
+    }
+
+    usernameChange(event) {
+        this.setState({username: event.target.value,});
+    }
+
+    passwordChange(event) {
+        this.setState({password: event.target.value,});
+    }
 
     render() {
         return(
-            <form method="post" action="/auth/register">
                 <div className="window">
                     <h3>Форма для регистрации</h3>
-                    <input type="text" placeholder="Введите логин"></input>
-                    <input type="password" placeholder="пароль"></input>
-                    <button type="submit">Зарегистрироваться</button>
+                    <input type="text" placeholder="Введите логин" onChange={this.usernameChange} value={this.state.username}></input>
+                    <input type="password" placeholder="пароль" onChange={this.passwordChange} value={this.state.password}></input>
+                    <button type="button" onClick={this.ClickButton}>Зарегистрироваться</button>
                 </div>
-            </form>
         );
     }
 }
--- src/main/java/ru/leshchenko/webresumeblog/repo/UserRepository.java
+++ src/main/java/ru/leshchenko/webresumeblog/repo/UserRepository.java
@@ -6,4 +6,5 @@ import ru.leshchenko.webresumeblog.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
