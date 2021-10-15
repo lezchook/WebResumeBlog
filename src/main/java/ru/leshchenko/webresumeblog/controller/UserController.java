4a2fdae51b0a23e9ea587c1887be7bd71fa17d34
Sergei Leshchenko <leshchenko.s2002@gmail.com>
2021-10-15 16:02:02 +0300

    Make Post service class.

--- src/main/java/ru/leshchenko/webresumeblog/controller/UserController.java
+++ src/main/java/ru/leshchenko/webresumeblog/controller/UserController.java
@@ -1,28 +1,21 @@
 package ru.leshchenko.webresumeblog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.leshchenko.webresumeblog.domain.Post;
-import ru.leshchenko.webresumeblog.repo.PostRepository;
+import ru.leshchenko.webresumeblog.service.PostService;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
-    PostRepository postRepository;
+    PostService postService;
 
     @PostMapping("/post")
-    public void setPost(@RequestBody HashMap<String, String> post ) {
-        Post post_req = new Post();
-        post_req.setId(new Long(post.get("id")));
-        post_req.setDescription(post.get("description"));
-        post_req.setTitle(post.get("title"));
-        postRepository.save(post_req);
+    public void setPost(@RequestBody Post post ) {
+        postService.savePost(post);
     }
 }
---
+++ src/main/java/ru/leshchenko/webresumeblog/service/PostService.java
@@ -0,0 +1,30 @@
+package ru.leshchenko.webresumeblog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.leshchenko.webresumeblog.domain.Post;
+import ru.leshchenko.webresumeblog.repo.PostRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    PostRepository postRepository;
+
+    public List<Post> getAllPost() {
+        List<Post> posts = new ArrayList<>();
+        postRepository.findAll().forEach(post -> posts.add(post));
+        return posts;
+    }
+
+    public void savePost(Post post) {
+        postRepository.save(post);
+    }
+
+    public void delete(Long id) {
+        postRepository.deleteById(id);
+    }
+}
